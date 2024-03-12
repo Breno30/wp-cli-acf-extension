9a62e259023e0fee444330cad9cc2f77b007f708
breno30 <brenodonascimentosilvaa@gmail.com>
2024-03-12 20:54:47 -0300

    declare command

--- installer.php
+++ installer.php
@@ -5,6 +5,50 @@ if (!defined('WP_CLI') || !class_exists('WP_CLI_Command') ) {
     return;
 }
 
-WP_CLI::add_command('acf import-layout', function ($args, $assoc_args) {
-    WP_CLI::success("Hi there!");
-});
+
+class ACF_Extension_WP_CLI extends WP_CLI_Command
+{
+    public function import($args, $assoc_args)
+    {
+        if (empty($args)) {
+            WP_CLI::error("Please inform the layout path, like so:\n wp acf import path-to-layout");
+        }
+
+        $path = $args[0];
+        $fullPath = realpath(ABSPATH . $path);
+
+        if (!is_readable($fullPath)) {
+            WP_CLI::error(sprintf('Import file missing or not readable: %s', $fullPath));
+        }
+
+        $json = file_get_contents($fullPath);
+        $json = json_decode($json, true);
+
+        if (!$json || !is_array($json)) {
+            return acf_add_admin_notice(__('Import file empty', 'acf'), 'warning');
+        }
+
+        $layoutKey = $json['key'];
+        $layoutName = $json['label'];
+
+        // Find ACF group
+        $groupList = acf_get_field_groups();
+        $fieldGroup = $groupList[0];
+
+        // Find ACF field
+        $fieldGroup = acf_get_field_group($fieldGroup['key']);
+        $fieldGroup['fields'] = acf_get_fields($fieldGroup);
+        $fieldGroup = acf_prepare_field_group_for_export($fieldGroup);
+        $post = acf_get_field_group_post($fieldGroup['key']);
+        $fieldGroup['ID'] = $post->ID;
+
+        // Set layout data
+        $fieldGroup['fields'][0]['layouts'][$layoutKey] = $json;
+
+        acf_import_field_group($fieldGroup);
+
+        WP_CLI::success("Layout '$layoutName' added successfully!");
+    }
+}
+
+WP_CLI::add_command('acf', 'ACF_Extension_WP_CLI');
