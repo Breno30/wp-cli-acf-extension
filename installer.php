692e0df26593c257f11a6f770a0e6271cb2cdaba
breno30 <brenodonascimentosilvaa@gmail.com>
2024-03-13 07:16:41 -0300

    create export command

--- installer.php
+++ installer.php
@@ -53,6 +53,52 @@ class ACF_Extension_WP_CLI extends WP_CLI_Command
 
         WP_CLI::success("Layout '$layoutName' added successfully!");
     }
+
+    public function export($args, $assoc_args)
+    {
+        // Find ACF group
+        $groupList = acf_get_field_groups();
+        $fieldGroup = $groupList[0];
+
+        // Find ACF field
+        $fieldGroup = acf_get_field_group($fieldGroup['key']);
+        $fieldGroup['fields'] = acf_get_fields($fieldGroup);
+        $fieldGroup = acf_prepare_field_group_for_export($fieldGroup);
+
+        // Inform layouts to user
+        $layouts = $fieldGroup['fields'][0]['layouts'];
+        WP_CLI\Utils\format_items('table', $layouts, array('label', 'name'));
+        $layoutName = $this->ask(' Inform the name of your layout:');
+
+        // Get layout by name
+        $layouts = array_filter($layouts, function ($item) use ($layoutName) {
+            return $item['name'] == $layoutName;
+        });
+
+        if (empty($layouts)) {
+            WP_CLI::error("Layout named '$layoutName' not found");
+        }
+
+        reset($layouts);
+        $layout = $layouts;
+
+        // Write layout json to file
+        $content = json_encode($layout, JSON_PRETTY_PRINT);
+        $fileName = "layout-$layoutName.json";
+        $fp = fopen(ABSPATH . "/$fileName", "wb");
+        fwrite($fp, $content);
+        fclose($fp);
+
+        WP_CLI::success("Layout saved successfully as $fileName");
+    }
+
+    protected function ask($question)
+    {
+        // Adding space to question and showing it.
+        fwrite(STDOUT, $question . '  ');
+
+        return strtolower(trim(fgets(STDIN)));
+    }
 }
 
 WP_CLI::add_command('acf', 'ACF_Extension_WP_CLI');
